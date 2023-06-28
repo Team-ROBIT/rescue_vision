0bed40386083ca1d40263e6f1376a77ddd9ed9f8
Lee Min Seok <0311ben@kw.ac.kr>
2023-06-28 13:23:37 +0900

    Update victimboard.cpp

--- src/victimboard.cpp
+++ src/victimboard.cpp
@@ -773,6 +773,7 @@ namespace vision_rescue
                     int baseline;
                     auto label_bg_sz = cv::getTextSize(
                         label.c_str(), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, 1, &baseline);
+                     if((rect.y - label_bg_sz.height - baseline - 10)>=0){
                     cv::rectangle(
                         Captured_Image_RGB,
                         cv::Point(rect.x, rect.y - label_bg_sz.height - baseline - 10),
@@ -780,35 +781,19 @@ namespace vision_rescue
                     cv::putText(Captured_Image_RGB, label.c_str(),
                                 cv::Point(rect.x, rect.y - baseline - 5),
                                 cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0, 0, 0));
+                    }
+                    else{
+                        cv::rectangle(
+                            Captured_Image_RGB,
+                            cv::Point(rect.x, rect.y+rect.height),//rect.y+rect.height
+                            cv::Point(rect.x + label_bg_sz.width, rect.y+rect.height+label_bg_sz.height+baseline+10), color, cv::FILLED);
+                        cv::putText(Captured_Image_RGB, label.c_str(),
+                                    cv::Point(rect.x, rect.y+rect.height + baseline + 5),
+                                    cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(0, 0, 0));
+                    }
                 }
             }
         }
-
-        auto total_end = std::chrono::steady_clock::now();
-
-        float inference_fps =
-            1000.0 /
-            std::chrono::duration_cast<std::chrono::milliseconds>(dnn_end - dnn_start)
-                .count();
-        float total_fps =
-            1000.0 / std::chrono::duration_cast<std::chrono::milliseconds>(
-                         total_end - total_start)
-                         .count();
-        std::ostringstream stats_ss;
-        stats_ss << std::fixed << std::setprecision(2);
-        stats_ss << "Inference FPS: " << inference_fps
-                 << ", Total FPS: " << total_fps;
-        auto stats = stats_ss.str();
-
-        int baseline;
-        auto stats_bg_sz = cv::getTextSize(
-            stats.c_str(), cv::FONT_HERSHEY_COMPLEX_SMALL, 1, 1, &baseline);
-        cv::rectangle(Captured_Image_RGB, cv::Point(0, 0),
-                      cv::Point(stats_bg_sz.width, stats_bg_sz.height + 10),
-                      cv::Scalar(0, 0, 0), cv::FILLED);
-        cv::putText(Captured_Image_RGB, stats.c_str(),
-                    cv::Point(0, stats_bg_sz.height + 5),
-                    cv::FONT_HERSHEY_COMPLEX_SMALL, 1, cv::Scalar(255, 255, 255));
     }
 
     bool Victimboard::isRectOverlapping(const cv::Rect &rect1,
